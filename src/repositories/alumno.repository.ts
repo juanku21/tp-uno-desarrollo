0943ad25005b950e2e91cea15a501967f4281b4e
TomasL <limeres110tomas@gmail.com>
2025-05-27 15:52:31 -0300

    Servicio y Repositorio de alumno

--- src/repositories/alumno.repository.ts
+++ src/repositories/alumno.repository.ts
@@ -1,7 +1,7 @@
 
 import { Prisma, Alumno } from "@prisma/client"
 import { BaseRepository } from "./base.repository"
-import prisma from "config/client"
+import prisma from "../config/client"
 
 export class AlumnoRepository extends BaseRepository 
 <typeof prisma.alumno, Alumno, Prisma.AlumnoCreateInput, Prisma.AlumnoUpdateInput> {
@@ -11,21 +11,18 @@ export class AlumnoRepository extends BaseRepository
     }
 
     public async getById(id : number) {
-        // try {
-        //     const result = await prisma.university.findUnique({
-        //         where: {
-        //             id: id
-        //         },
-        //         include: {
-        //             faculty: true
-        //         }
-        //     })
+        try {
+            const result = await prisma.alumno.findUnique({
+                where: {
+                    alumno: id
+                }
+            })
 
-        //     return result
-        // } 
-        // catch (error : any) {
-        //     throw new Error(`Error al leer la base de datos: ${error}`)
-        // }
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`Error al leer la base de datos: ${error}`)
+        }
     }
 
 }
--- src/services/alumno.service.ts
+++ src/services/alumno.service.ts
@@ -1,28 +1,73 @@
 
+import { AlumnoRepository } from "../repositories/alumno.repository"
+import { Alumno, Prisma } from "@prisma/client"
 
-import { AlumnoRepository } from "repositories/alumno.repository";
-
+const repository = new AlumnoRepository()
 
 export class AlumnoService {
 
-    public static async get() {
-
+    public static async get() : Promise<Alumno[]> {
+        try {
+            const result = await repository.get()
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible obtener la lista de alumnos ${error}`)
+        }
     }
 
-    public static async findById() {
-
+    public static async findById(id : number) : Promise<Alumno | null> {
+        try {
+            const result = await repository.getById(id)
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible obtener el alumno ${error}`)
+        }
     }
 
-    public static async create() {
-
+    public static async create(alumno : Prisma.AlumnoCreateInput) : Promise<Alumno> {
+        try {
+            const result = await repository.create(alumno)
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible crear un nuevo alumno ${error}`)
+        }
     }
 
-    public static async update() {
+    public static async update(id : number, alumno : Prisma.AlumnoUpdateInput) : Promise<Alumno> {
+        try {
+            const exists = await repository.getById(id)
 
+            if (exists === null) {
+                throw new Error("El alumno que desea actualizar no existe")
+            }
+
+            const result = await repository.update(id, alumno)
+
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible actualizar el alumno ${error}`)
+        }
     }
 
-    public static async delete() {
+    public static async delete(id : number) : Promise<Alumno> {
+        try {
+            const exists = await repository.getById(id)
 
+            if (exists === null) {
+                throw new Error("El alumno que desea eliminar no existe")
+            }
+
+            const result = await repository.delete(id)
+
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible eliminar el alumno ${error}`)
+        }
     }
 
 }
