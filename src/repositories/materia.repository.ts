bc40eb39175c9c156c76330a8dd0d490646d3690
Macarena Damiani <macarenadamiani@gmail.com>
2025-05-28 09:42:45 -0300

    repositorio y servicio de materia realizado

--- src/repositories/materia.repository.ts
+++ src/repositories/materia.repository.ts
@@ -1,2 +1,29 @@
 
+import { Prisma, Materia } from "@prisma/client"
+import { BaseRepository } from "./base.repository"
+import prisma from "../config/client"
+
+export class MateriaRepository extends BaseRepository 
+<typeof prisma.materia, Materia, Prisma.MateriaCreateInput, Prisma.MateriaUpdateInput> {
+
+    constructor(){
+        super(prisma.materia)
+    }
+
+    public async getById(id : number) : Promise<Materia | null> {
+        try {
+            const result = await prisma.materia.findUnique({
+                where: {
+                    materia: id 
+                }
+            })
+
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`Error al leer la base de datos: ${error}`)
+        }
+    }
+
+}
 
